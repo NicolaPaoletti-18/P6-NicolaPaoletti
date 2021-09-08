a21f6a6c6aaec28b6a6f4971ff41bcc9df73a977
Nicola Paoletti <nicola97pao@hotmail.it>
2021-09-08 12:44:04 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+// emporter
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+const Thing = require('./models/Thing');
+const userRoutes = require('./routes/users');
+
+
+mongoose.connect('mongodb+srv://jimbob:NICOLA18@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+app.use(bodyParser.json());
+
+app.post('/api/sauces', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/sauces/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.use('/api/sauces', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
+
+//app.use('/api/sauces', Thing);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+const userCtrl = require('../constrollers/users');
+
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+
+module.exports = router;
